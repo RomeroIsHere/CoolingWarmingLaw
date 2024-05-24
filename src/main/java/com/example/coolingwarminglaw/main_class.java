33d31ff0d041117bdf2329c36406d1a00aaffb0d
ViraBot50 <josebrayansaldanaaguado@gmail.com>
2024-05-23 19:15:07 -0600

    Initial commit

--- src/main/java/com/example/coolingwarminglaw/main_class.java
+++ src/main/java/com/example/coolingwarminglaw/main_class.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication1 extends Application {
+public class main_class extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication1.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(main_class.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
